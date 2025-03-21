7dd726b862d3d43aa1a30721052b5f7d309eb825
Jonas Andersson <jonas.andersson.web@gmail.com>
2025-03-22 00:01:00 +0100

    Ran format with the new config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,36 @@
 <script lang="ts">
-	import * as m from '$lib/paraglide/messages.js';
-	import profileImg from '$lib/assets/images/profile_pic.jpg';
+    import * as m from '$lib/paraglide/messages.js';
+    import profileImg from '$lib/assets/images/profile_pic.jpg';
 
-	let showMenu = $state(false);
+    let showMenu = $state(false);
 </script>
 
 <main class="flex h-full flex-col items-center justify-center p-2">
-	<h1 class="mb-3">Jonas Andersson</h1>
-	<span class="text-xl">{m.page_sub_title()}</span>
+    <h1 class="mb-3">Jonas Andersson</h1>
+    <span class="text-xl">{m.page_sub_title()}</span>
 
-	<div class="mt-8">
-		<button aria-label="Visa menyn" onclick={() => (showMenu = !showMenu)}>
-			<img class="aspect-square w-52 rounded-full" src={profileImg} alt={m.profile_img_alt()} />
-		</button>
-	</div>
+    <div class="mt-8">
+        <button aria-label="Visa menyn" onclick={() => (showMenu = !showMenu)}>
+            <img
+                class="aspect-square w-52 rounded-full"
+                src={profileImg}
+                alt={m.profile_img_alt()} />
+        </button>
+    </div>
 
-	{#if !showMenu}
-		<nav>
-			<ul class="mt-8 flex items-center justify-center gap-8">
-				<li>
-					<a href="/">{m.navigation_home()}</a>
-				</li>
-				<li>
-					<a href="/about">{m.navigation_about()}</a>
-				</li>
-				<li>
-					<a href="/portfolio">{m.navigation_portfolio()}</a>
-				</li>
-			</ul>
-		</nav>
-	{/if}
+    {#if !showMenu}
+        <nav>
+            <ul class="mt-8 flex items-center justify-center gap-8">
+                <li>
+                    <a href="/">{m.navigation_home()}</a>
+                </li>
+                <li>
+                    <a href="/about">{m.navigation_about()}</a>
+                </li>
+                <li>
+                    <a href="/portfolio">{m.navigation_portfolio()}</a>
+                </li>
+            </ul>
+        </nav>
+    {/if}
 </main>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import SubPage from '$lib/components/composites/sub-page/sub-page.svelte';
-	import * as m from '$lib/paraglide/messages.js';
+    import SubPage from '$lib/components/composites/sub-page/sub-page.svelte';
+    import * as m from '$lib/paraglide/messages.js';
 </script>
 
 <main class="flex h-full flex-col items-center justify-center p-2">
-	<SubPage>
-		<h1>{m.navigation_about()}</h1>
-	</SubPage>
+    <SubPage>
+        <h1>{m.navigation_about()}</h1>
+    </SubPage>
 </main>
