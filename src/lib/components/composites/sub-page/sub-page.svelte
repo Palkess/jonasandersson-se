43b50a66941d7d063a6a66054fd39f4a0f58ec82
Jonas Andersson <jonas.andersson.web@gmail.com>
2025-03-22 00:00:06 +0100

    Added some foundation for setting up sub pages

---
+++ src/lib/components/composites/sub-page/sub-page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import * as m from '$lib/paraglide/messages.js';
+    import type { Snippet } from 'svelte';
+    interface Props {
+        children: Snippet;
+    }
+
+    let { children }: Props = $props();
+</script>
+
+<div class="relative container rounded border bg-white px-8 py-8 text-black shadow">
+    <button
+        class="absolute -top-8 -left-8 aspect-square w-16 rounded-full border bg-black text-white"
+        onclick={() => history.back()}>
+        Back
+    </button>
+    {@render children()}
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import SubPage from '$lib/components/composites/sub-page/sub-page.svelte';
+	import * as m from '$lib/paraglide/messages.js';
+</script>
+
+<main class="flex h-full flex-col items-center justify-center p-2">
+	<SubPage>
+		<h1>{m.navigation_about()}</h1>
+	</SubPage>
+</main>
