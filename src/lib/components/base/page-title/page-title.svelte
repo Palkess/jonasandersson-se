8ed0aa6fde3f22015217858bd82f610c53c3fcb3
Jonas Andersson <jonas.andersson.web@gmail.com>
2025-03-22 14:54:27 +0100

    Added new page title component to be used for pages h1 title

---
+++ src/lib/components/base/page-title/page-title.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+    import { twMerge } from 'tailwind-merge';
+
+    interface Props {
+        class?: string;
+        children: Snippet;
+    }
+
+    let { class: className = '', children }: Props = $props();
+</script>
+
+<h1
+    class={twMerge(
+        "relative mb-24 inline-block after:absolute after:-bottom-9 after:block after:h-4 after:w-3/4 after:rounded after:bg-white after:content-['']",
+        className
+    )}>
+    {@render children()}
+</h1>
