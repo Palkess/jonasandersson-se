74ed4b7033dbde4383e21dcddc322331f5611a0e
Jonas Andersson <jonas.andersson.web@gmail.com>
2025-03-22 14:53:15 +0100

    Added topmenu to switch languages

---
+++ src/lib/components/foundation/icons/FlagSwedenIcon.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { twMerge } from 'tailwind-merge';
+    import { type IconProps } from './common';
+
+    let { class: className = '' }: IconProps = $props();
+</script>
+
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    id="flag-icons-se"
+    viewBox="0 0 640 480"
+    class={twMerge('w-8', className)}>
+    <path fill="#005293" d="M0 0h640v480H0z" />
+    <path fill="#fecb00" d="M176 0v192H0v96h176v192h96V288h368v-96H272V0z" />
+</svg>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,33 @@
     import { i18n } from '$lib/i18n';
     import { ParaglideJS } from '@inlang/paraglide-sveltekit';
     import '../app.css';
+    import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
+    import { goto } from '$app/navigation';
+    import { page } from '$app/state';
     let { children } = $props();
+    import FlagSwedenIcon from '$lib/components/foundation/icons/FlagSwedenIcon.svelte';
+    import FlagBritainIcon from '$lib/components/foundation/icons/FlagBritainIcon.svelte';
+
+    function switchToLanguage(newLanguage: AvailableLanguageTag) {
+        const canonicalPath = i18n.route(page.url.pathname);
+        const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage);
+        goto(localisedPath);
+    }
 </script>
+
+<div class="container mx-auto mt-2 flex items-center justify-end gap-4">
+    <button class="flex gap-2" onclick={() => switchToLanguage('en')}><FlagBritainIcon />en</button>
+    <button class="flex gap-2" onclick={() => switchToLanguage('sv')}><FlagSwedenIcon /> sv</button>
+</div>
 
 <ParaglideJS {i18n}>
     {@render children()}
 </ParaglideJS>
+
+<footer>
+    <div class="container mx-auto mt-8">
+        <p class="text-center text-sm text-gray-500">
+            Copyright © {new Date().getFullYear()} Jonas Andersson
+        </p>
+    </div>
+</footer>
