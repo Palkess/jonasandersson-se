803c10952719aff591405fd399b77cc3eb92666f
Jonas Andersson <jonas.andersson.web@gmail.com>
2025-03-22 17:29:46 +0100

    Reworked template a bit and removed the idea I had of making some sort of popover menu. Saving that for the future, focusing on MVP features

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,6 @@
     import * as m from '$lib/paraglide/messages.js';
     import profileImg from '$lib/assets/images/profile_pic.jpg';
     import ExpertiseList from '$lib/components/base/expertise-list/expertise-list.svelte';
-
-    let showMenu = $state(false);
 </script>
 
 <main class="flex h-full flex-col items-center justify-center p-2">
@@ -12,27 +10,21 @@
     <ExpertiseList />
 
     <div class="mt-8">
-        <button aria-label="Visa menyn" onclick={() => (showMenu = !showMenu)}>
-            <img
-                class="view-transition-profileImg aspect-square w-52 rounded-full border-2 object-cover p-2"
-                src={profileImg}
-                alt={m.profile_img_alt()} />
-        </button>
+        <img
+            class="view-transition-profileImg aspect-square w-52 rounded-full border-2 object-cover p-2"
+            src={profileImg}
+            alt={m.profile_img_alt()}
+            draggable="false" />
     </div>
 
-    {#if !showMenu}
-        <nav>
-            <ul class="mt-8 flex items-center justify-center gap-8">
-                <li>
-                    <a href="/">{m.navigation_home()}</a>
-                </li>
-                <li>
-                    <a href="/about">{m.navigation_about()}</a>
-                </li>
-                <li>
-                    <a href="/portfolio">{m.navigation_portfolio()}</a>
-                </li>
-            </ul>
-        </nav>
-    {/if}
+    <nav>
+        <ul class="mt-8 flex items-center justify-center gap-8 text-xl font-medium">
+            <li>
+                <a href="/about">{m.navigation_about()}</a>
+            </li>
+            <li>
+                <a href="/portfolio">{m.navigation_portfolio()}</a>
+            </li>
+        </ul>
+    </nav>
 </main>
